d6fccc4c095c75d72ac3c64cf07f0a5fc623c60c
aravind30122003 <aravind30122003@gmail.com>
2025-07-28 02:12:27 +0530

    files were added

---
+++ app/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const message = `
+    🌸✨ Hi Muu! ✨🌸
+
+    🎂 Advance Happy Birthday! 🎉
+
+    ❤️ I love you so much — Umma 💋
+
+    🕊️ Wishing you happiness, health, and all your dreams come true!
+
+    — With love from Aarav 💌
+  `;
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end(message);
+});
+
+server.listen(port, () => {
+  console.log(`🌐 Server running at http://localhost:${port}`);
+});
+
